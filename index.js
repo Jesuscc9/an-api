54136abf8dafed30c323d6dcf698300f0c52d290
Jesús Cervantes <jesus.cervantes0801@gmail.com>
2021-11-03 04:51:56 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('Hola gente');
+})
+
+const PORT = 3001;
+
+app.listen(PORT);
+
+console.log(`Server running on port ${PORT}`);
